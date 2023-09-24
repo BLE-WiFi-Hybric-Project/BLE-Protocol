265a7551127656e1ae3cee96f54c30a9fb999e66
HieuDinh3322 <145694591+HieuDinh3322@users.noreply.github.com>
2023-09-24 21:35:10 +0700

    Update Peripheral.ino
    Commit day 24/09

--- Peripheral/Peripheral.ino
+++ Peripheral/Peripheral.ino
@@ -3,24 +3,35 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLECharacteristic *pCharacteristic;
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
 bool buttonState = false;
 
-const int buttonPin = 2;  // Define the pin for the button
+const int buttonPin = 2;
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      // When a client connects
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+      // When a client disconnects
+    }
+};
 
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  BLEDevice::init("ESP32_Peripheral");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(BLEUUID("2185cd30-5935-11ee-8c99-0242ac120002"));
+  BLEDevice::init("ButtonServer");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(BLEUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b"));
 
   pCharacteristic = pService->createCharacteristic(
-                      BLEUUID("2185cd30-5935-11ee-8c99-0242ac120002"),
+                      BLEUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8"),
                       BLECharacteristic::PROPERTY_READ |
-                      BLECharacteristic::PROPERTY_WRITE |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
+                      BLECharacteristic::PROPERTY_WRITE
                     );
 
   pCharacteristic->addDescriptor(new BLE2902());
@@ -28,17 +39,19 @@ void setup() {
   pService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(BLEUUID("2185cd30-5935-11ee-8c99-0242ac120002"));
-  pAdvertising->setScanResponse(false);
+  pAdvertising->addServiceUUID(pService->getUUID());
+  pAdvertising->setScanResponse(true);
   pAdvertising->start();
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState) {
-    pCharacteristic->setValue((uint8_t*)&buttonState, 1);
+  if (buttonState == LOW) {
+    pCharacteristic->setValue("1");
     pCharacteristic->notify();
-    delay(1000);
+    delay(1000); // Delay to avoid sending too frequently
   }
+
+  delay(10);
 }
